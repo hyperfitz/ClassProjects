c93bb9208ea655a2879cb4cd20c0fb296d9269ac
hyperfitz <hyperfitz@users.noreply.github.com>
2025-08-21 13:47:47 -0700

    Just done for the moment

---
+++ Salines_Node.js
@@ -0,0 +1,60 @@
+const WS = require('ws');
+
+const PORT = 3002;
+const MY_ADDRESS = "ws://localhost:3002";
+const server = new WS.Server({ port:PORT });
+
+let opened = [], connected = [];
+
+console.log("Saline listening on PORT" , PORT); 
+
+server.on("connection", (socket) => {
+    socket.on("messages", message => {
+        const _message = JSON.parse(message);
+        console.log(_message);
+
+        switch(_message.type) {
+            case "TYPE_HANDSHAKE":
+                const nodes = _message.data;
+                nodes.forEach(node => connect(node));
+
+        }
+    })
+})
+
+function connect (address) {
+    if (!connected.find(peerAddress => peerAddress === address) && address !== MY_ADDRESS) {
+        const socket = new WS(address);
+
+        socket.on("open", () => {
+            socket.send(JSON.stringify(produceMessage("TYPE_HANDSHAKE", [MY_ADDRESS, ...connected])));
+
+            opened.forEach(node => node.socket.send(JSON.stringify(produceMessage("TYPE_HANDSHAKE", [address]))));
+
+            if (!opened.find(peer => peer.address === address) && address !== MY_ADDRESS) {
+                opened.push({ socket, address });
+                connected.push(address);
+            }
+        });
+
+        socket.on("close", () => {
+            opened.splice(connected.indexOf(address), 1);
+            connected.splice(connected.indexOf(address), 1);
+        });
+    }
+}
+
+function produceMessage (type, data) {
+    return { type, data };
+}
+
+function sendMessage (message) {
+    opened.forEach(node => {
+        node.socket.send(JSON.stringify(message));
+    })
+}
+
+setTimeout(() => {
+    sendMessage(produceMessage("MESSAGE", "Hello from Saline!"));
+    
+}, 15000);
---
+++ minor.js
@@ -0,0 +1,63 @@
+const WS = require('ws');
+
+const PORT = 3000;
+const PEERS = ["ws://localhost:3001", "ws://localhost:3002"];
+const MY_ADDRESS = "ws://localhost:3000";
+const server = new WS.Server({ port:PORT });
+
+let opened = [], connected = [];
+
+console.log("miner listening on PORT" , PORT); 
+
+server.on("connection", (socket) => {
+    socket.on("messages", message => {
+        const _message = JSON.parse(message);
+        console.log(_message);
+
+        switch(_message.type) {
+            case "TYPE_HANDSHAKE":
+                const nodes = _message.data;
+                nodes.forEach(node => connect(node));
+
+        }
+    })
+})
+
+function connect (address) {
+    if (!connected.find(peerAddress => peerAddress === address) && address !== MY_ADDRESS) {
+        const socket = new WS(address);
+
+        socket.on("open", () => {
+            socket.send(JSON.stringify(produceMessage("TYPE_HANDSHAKE", [MY_ADDRESS, ...connected])));
+
+            opened.forEach(node => node.socket.send(JSON.stringify(produceMessage("TYPE_HANDSHAKE", [address]))));
+
+            if (!opened.find(peer => peer.address === address) && address !== MY_ADDRESS) {
+                opened.push({ socket, address });
+                connected.push(address);
+            }
+        });
+
+        socket.on("close", () => {
+            opened.splice(connected.indexOf(address), 1);
+            connected.splice(connected.indexOf(address), 1);
+        });
+    }
+}
+
+function produceMessage (type, data) {
+    return { type, data };
+}
+
+function sendMessage (message) {
+    opened.forEach(node => {
+        node.socket.send(JSON.stringify(message));
+    })
+}
+
+PEERS.forEach(peer => connect(peer));
+
+setTimeout(() => {
+    sendMessage(produceMessage("MESSAGE", "Hello from miner!"));
+    
+}, 5000);
